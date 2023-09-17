cfcfb0edf36142b54d2ac9e0df2af0fccd691903
PON Lyhong <lyhong.pon@gmail.com>
2023-09-17 20:36:17 +0700

    feat: dashboard

--- web/src/views/Dashboard.vue
+++ web/src/views/Dashboard.vue
@@ -1,3 +1,37 @@
+<script setup>
+import gql from "graphql-tag";
+import { useQuery } from "@vue/apollo-composable";
+
+const { result } = useQuery(gql`
+  query {
+    users_count
+    customers_count
+    purchases_count
+  }
+`);
+</script>
+
 <template>
-  <div>Hello world</div>
+  <div id="dashboard">
+    <CRow>
+      <CCol :xs="3">
+        <CWidgetStatsA class="mb-4" color="primary">
+          <template #value>{{ result?.users_count || 0 }} </template>
+          <template #title><p>Users</p></template>
+        </CWidgetStatsA>
+      </CCol>
+      <CCol :xs="3">
+        <CWidgetStatsA class="mb-4" color="info">
+          <template #value>{{ result?.customers_count || 0 }}</template>
+          <template #title><p>Customers</p></template>
+        </CWidgetStatsA>
+      </CCol>
+      <CCol :xs="3">
+        <CWidgetStatsA class="mb-4" color="danger">
+          <template #value>{{ result?.purchases_count || 0 }} </template>
+          <template #title><p>Purchases</p></template>
+        </CWidgetStatsA>
+      </CCol>
+    </CRow>
+  </div>
 </template>
